1ce84e583be21f1bec3091a617020e89df444a3f
Ikhun Um <ih.pert@gmail.com>
2023-08-04 21:22:18 +0900

    Upload gradle build scan
    Motivation:
    
    It must be useful to see test results on a website and share the link
    with other folks. Since Gradle enterprise is activated in the Armeria
    project, we can upload build scans to https://ge.armeria.dev.
    
    An access token is required to upload build scans. As GitHub secrets are
    not exposed to the pull requests from public forks, it is not possible to
    directly upload the build scans in a CI build workflow.
    
    @ribafish suggested uploading `build-scan-data` to GitHub Actions
    artifacts and then publishing the backup scan data in a sperate workflow
    run on Armeria repository.
    
    Modifications:
    
    - Added a new step that uploads Gradle build scan data to GitHub Actions
      artifacts to the CI build.
    - Redefined the build job name as a deterministic name.
      - A job name is used to fetch the job result from the JOB API.
      - GitHub does not provide a way to get a job ID or a job name in a workflow,
        - https://github.com/orgs/community/discussions/8945
        - https://stackoverflow.com/questions/71593861/how-to-get-github-actions-matrix-job-name-within-job
    - Added `gradle-build-scan.yml` workflow which is triggered when a check
      completes.
      - A sleep time is intentionlly added in the workflow because artifacts
        are not as `check_run` is triggered.
      - Scan results are reported as a comment on a pull request.
    
    Result:
    
    You can now see Gradle build scan of a pull request on https://ge.armeria.dev

---
+++ .github/actions/comment-build-scan.ts
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2023 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { Octokit } from 'octokit';
+
+main();
+
+async function main(): Promise<void> {
+  const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
+
+  const prNumber = parseInt(process.env.PR_NUMBER);
+  const scans = process.env.BUILD_SCANS.split("\n");
+  // TODO(ikhoon): Convert to line/armeria
+  const owner = 'ikhoon';
+  const repo = 'armeria';
+
+  const jobs = (await octokit.rest.actions.listJobsForWorkflowRun({
+    owner: owner,
+    repo: repo,
+    run_id: parseInt(process.env.RUN_ID),
+  })).data.jobs;
+
+  const comments = await octokit.rest.issues.listComments({
+    owner,
+    repo,
+    issue_number: prNumber,
+  })
+
+  let commentBody = `## 🔍 Gradle build scans (commit: ${process.env.SHA})\n\n`;
+  for (const scan of scans) {
+    // scan string pattern: "build-scan-<job-name> https://ge.armeria.dev/xxxxxx"
+    const tokens = scan.split(" ");
+    const jobName = tokens[0].replace("build-scan-", "")
+    const job = jobs.find(job => job.name === jobName);
+    const scanUrl = tokens[1].trim();
+    if (job.conclusion === 'success') {
+      commentBody += `✅ [${job.name}](${job.url}) - ${scanUrl}\n`;
+    } else {
+      commentBody += `❌ [${job.name}](${job.url}) (${job.conclusion}) - ${scanUrl}\n`;
+    }
+  }
+
+  const scanComment = comments.data.find(comment =>
+    comment.user.login === "github-actions[bot]" && comment.body.includes('Gradle build scans'))
+  if (scanComment) {
+    // Update the previous comment
+    await octokit.rest.issues.updateComment({
+      owner,
+      repo,
+      comment_id: scanComment.id,
+      body: commentBody
+    })
+  } else {
+    // If no previous comment, create a new one
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: prNumber,
+      body: commentBody
+    })
+  }
+}
